d7ca89db21708de609974503a226b90a3a158af4
Guilherme <guilhermekappaun@gmail.com>
2023-12-05 06:40:47 -0300

    Projeto 3

--- src/dao/generic/jdbc/dao/IClienteDAO.java
+++ src/dao/generic/jdbc/dao/IClienteDAO.java
@@ -1,2 +1,13 @@
-package dao.generic.jdbc.dao;public class IClienteDAO {
+package dao.generic.jdbc.dao;
+
+import domain.Cliente;
+
+import java.util.List;
+
+public interface IClienteDAO {
+    public Integer cadastrar(Cliente cliente) throws Exception;
+    public Integer atualizar(Cliente cliente) throws Exception;
+    public Cliente buscar(String codigo) throws Exception;
+    public List<Cliente> buscarTodos() throws Exception;
+    public Integer excluir(Cliente cliente) throws Exception;
 }
--- test/ClienteTest.java
+++ test/ClienteTest.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class ClienteTest {
+import dao.generic.jdbc.dao.ClienteDAO;
+import dao.generic.jdbc.dao.IClienteDAO;
+import domain.Cliente;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ClienteTest {
+    @Test
+    public void cadastrarTest() throws Exception {
+        IClienteDAO dao = new ClienteDAO();
+
+        Cliente cliente = new Cliente();
+        cliente.setCodigo("01");
+        cliente.setNome("Guilherme");
+
+        Integer qtd = dao.cadastrar(cliente);
+        assertTrue(qtd ==1);
+
+        Cliente clienteBD = dao.buscar(cliente.getCodigo());
+        assertNotNull(clienteBD);
+        assertNotNull(clienteBD.getId());
+        assertEquals(cliente.getCodigo(), clienteBD.getCodigo());
+        assertEquals(cliente.getNome(), clienteBD.getNome());
+
+        Integer qtdDe1 = dao.excluir(clienteBD);
+        assertNotNull(qtdDe1);
+    }
 }
